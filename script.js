04030ecbb933a5b1cef57d222aaef62a58e29ff0
rishikrishna <r.rishikrishna@gmail.com>
2021-07-29 14:05:58 +0530

    functionality and bug fix
    functionality: empty last small cup\nbugfix: text color inside big cup

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ smallCups.forEach((cup, idx) => {
 })
 
 function highlightCup(idx) {
+    if(smallCups[idx].classList.contains("full") && !smallCups[idx + 1].classList.contains("full")){
+        idx--;
+    }
+
     smallCups.forEach((cup, idx2) => {
         if (idx2 <= idx) {
             cup.classList.add('full')
